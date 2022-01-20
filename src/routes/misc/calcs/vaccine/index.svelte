5e8dcc682cf4db0c401bd7a91b113e067aea8615
Dave Morgan <dave@dmorg.org>
2022-01-20 02:30:01 -0800

    vaccine calculator

--- src/routes/misc/calcs/vaccine/index.svelte
+++ src/routes/misc/calcs/vaccine/index.svelte
@@ -2,50 +2,49 @@
 	import Drop from '$lib/misc/Drop.svelte';
 	import Card from '$lib/Card.svelte';
 	import Sect from '$lib/numbering/Sect.svelte';
-	import Ki from '$lib/katex/Ki.svelte';
-	import Kd from '$lib/katex/Kd.svelte';
 
-	let popPercent = 80,
-		pop = 3000000,
-		icuPercent = 50,
-		icu = 300,
-        numVaccinated,
-        numUnvaccinated,
-        numVacICU,
-        likelyVac,
-        unvacPercent,
-        icuUnvacPercent,
-        numUnVacICU,        
-        likelyUnVac,
-        final;
+	let popPercent = 67.6,
+		pop = 100000,
+		icuPercent = 11.6,
+		icu = 14.8,
+		numVaccinated,
+		numUnvaccinated,
+		numVacICU,
+		likelyVac,
+		unvacPercent,
+		icuUnvacPercent,
+		numUnVacICU,
+		likelyUnVac,
+		final;
 
 	const calculate = () => {
 		popPercent = Number(popPercent);
-        pop = Number(pop);        
-        icuPercent = Number(icuPercent);
-        icu = Number(icu);
+		pop = Number(pop);
+		icuPercent = Number(icuPercent);
+		icu = Number(icu);
 
-        numVaccinated = pop*popPercent/100;
-        numVacICU = Number(icu)*icuPercent/100;
-        likelyVac = numVaccinated / numVacICU;
+		numVaccinated = Math.round((pop * popPercent) / 100);
+		numVacICU = (Number(icu) * icuPercent) / 100;
+		likelyVac = Math.round(numVaccinated / numVacICU);
 
-        unvacPercent = 100 - popPercent;
-        numUnvaccinated = unvacPercent*pop/100;
-        icuUnvacPercent = 100-icuPercent;
-        numUnVacICU = icuUnvacPercent * icu/100;
-        likelyUnVac = numUnvaccinated / numUnVacICU;
-        final = likelyVac / likelyUnVac;
-        
+		unvacPercent = (100 - popPercent).toPrecision(4);
+		numUnvaccinated = (unvacPercent * pop) / 100;
+		icuUnvacPercent = (100 - icuPercent).toPrecision(4);
+		numUnVacICU = ((icuUnvacPercent * icu) / 100).toPrecision(4);
+		likelyUnVac = Math.round(numUnvaccinated / numUnVacICU);
+		final = (likelyVac / likelyUnVac).toPrecision(4);
 	};
 	const processChange = () => {
 		calculate();
 	};
 
-    calculate();
+	calculate();
 </script>
 
 <main>
 	<Sect type="chapter0">Vaccine Efficacy Calculator</Sect>
+    <br/>
+    <p>(Click the downward pointing green triangles for more information.)</p>
 
 	<Card>
 		<Drop>
@@ -54,14 +53,14 @@
 			</section>
 			<section slot="drop">
 				<p>
-					Suppose that you hear in the news that <Ki>50\%</Ki> of people in ICU are vaccinated. Do you
-					wonder why the 'experts' want us all to get vaccinated? Then this calculator is for you.
+					Suppose that you hear in the news that 50% of Covid deaths are vaccinated. Do you wonder
+					why the 'experts' want us all to get vaccinated? Then this calculator is for you.
 				</p>
 
 				<p>
 					At first glance, it may seem that vaccination isn't helping at all; there are as many
-					vaccinated patients as unvaccinated patients. But are we really just as likely to end up
-					in the ICU if we do get vaccinated?
+					vaccinated patients as unvaccinated patients. But are we really just as likely to die if
+					we do get vaccinated?
 				</p>
 
 				<p>
@@ -70,72 +69,86 @@
 				</p>
 
 				<p>
-					Consider this scenario: <Ki>80\%</Ki> of the population are fully vaccinated and the fully
-					vaccinated make up half of the patients in ICU. Then, <Ki>20\%</Ki> of the population (the
-					unvaccinated) are taking up as much ICU space as the <Ki>80\%</Ki> of the population who are
-					vaccinated. So the vaccinated do have protection; with these figures, it looks like you are
-					four times more likely to end up in ICU if you are unvaccinated.
+					Consider this scenario: 80% of the population are fully vaccinated and the fully vaccinated make up half of Covid deaths. Then, 20% of the population (the
+					unvaccinated) have the same number of deaths as the 80% of the population who are vaccinated. So the vaccinated do have protection; with these figures, it looks like you are four times more likely to die if unvaccinated.
 				</p>
 
 				<p>
-					Let's work with some real numbers, do the calculations and find the real level of
-					protection. There is math here, but it's pretty simple.
+					Let's work with some real numbers, do the calculations and find the real level of protection. There is math here, but it's pretty simple.
 				</p>
 			</section>
 		</Drop>
 	</Card>
 
 	<Card>
-        <Drop>
-            <section slot='top'>
-                 <header>The Inputs</header>
-            </section>
-            <section slot='drop'>
-                These default values were retrieved from ....
+		<Drop>
+			<section slot="top">
+				<header>The Inputs</header>
+			</section>
+			<section slot="drop">
+				These default values are for Switzerland and were retrieved from <a
+					href="https://ourworldindata.org/covid-deaths-by-vaccination">Our World In Data</a
+				>
+				on 20th January, 2022. Scroll down to the data for Switzerland. Switzerland was chosen
+				because the data were the most recent for a country comparable to Canada or Britain. The
+				numbers are per 100,000 of the population.
 
-                <p>Feel free to enter your own numbers. Note that the only two figures required are the percentage of the population vaccinated and the percentage of vaccinated patients in ICU. The size of the population and the number of people in ICU do not affect the likelihood of ending up in ICU. See for yourself by randomly changing those values.</p>
-            </section>
-		   
-        </Drop>
+				<p>
+					Feel free to enter your own numbers. Note that the only two figures required are the
+					percentage of the population vaccinated and the percentage of vaccinated deaths.
+					The size of the population and the number of deaths do not affect the likelihood of dying. (See for yourself by randomly changing those values.)
+				</p>
+			</section>
+		</Drop>
 		<form on:submit|preventDefault={calculate}>
-            <label class="pop">
+			<label class="pop">
 				Size of the population:
 				<input type="number" step="any" required bind:value={pop} on:input={processChange} />
 			</label>
 			<label class="popPercent">
-				<Ki>\%</Ki> of the population vaccinated:
+				% of the population fully vaccinated:
 				<input type="number" step="any" required bind:value={popPercent} on:input={processChange} />
 			</label>
 			<label class="icu">
-				Number of ICU patients:
+				Number of Covid deaths:
 				<input type="number" step="any" required bind:value={icu} on:input={processChange} />
 			</label>
 			<label class="icu">
-				<Ki>\%</Ki> of ICU patients vaccinated:
+				% of Covid deaths that were vaccinated:
 				<input type="number" step="any" required bind:value={icuPercent} on:input={processChange} />
-			</label>			
+			</label>
 		</form>
 	</Card>
 
 	<Card>
 		<header>The Calculations</header>
 
-        <p>{popPercent}% of the population of {pop} are vaccinated, i.e. {numVaccinated} are vaccinated.</p>
+		<p>
+			{popPercent}% of the population of {pop} are vaccinated, i.e. {numVaccinated} are vaccinated.
+		</p>
 
-        <p>{icuPercent}% of the {icu} in the ICU are vaccinated, i.e. {numVacICU} vaccinated patients are in the ICU.</p>
+		<p>
+			{icuPercent}% of the {icu} deaths were vaccinated, i.e. {numVacICU} vaccinated patients died.
+		</p>
 
-        <p>The chance of a vaccinated person ending up in ICU is {numVacICU}/{numVaccinated}, i.e. <strong>1 in {likelyVac}</strong>.</p>
+		<p>
+			The likelihood of a vaccinated person dying is {numVacICU}/{numVaccinated}, i.e.
+			<strong>1 in {likelyVac}</strong>.
+		</p>
 
-        <p>{unvacPercent}% of the population of {pop} are unvaccinated, i.e. {numUnvaccinated} are not vaccinated.</p>
+		<p>
+			{unvacPercent}% of the population of {pop} are unvaccinated, i.e. {numUnvaccinated} are not vaccinated.
+		</p>
 
-        {icuUnvacPercent}% of the {icu} in the ICU are not vaccinated, i.e. {numUnVacICU} unvaccinated patients are in the ICU.
+		{icuUnvacPercent}% of the {icu} deaths were not vaccinated, i.e. {numUnVacICU} deaths are unvaccinated.
 
-        <p>The chance of a vaccinated person ending up in ICU is {numUnVacICU}/{numUnvaccinated}, i.e. <strong>1 in {likelyUnVac}</strong>.</p>
+		<p>
+			The likelihood of an unvaccinated person dying is {numUnVacICU}/{numUnvaccinated}, i.e.
+			<strong>1 in {likelyUnVac}</strong>.
+		</p>
 
-        Thus, an unvaccinated person is {likelyVac}/{likelyUnVac} = <strong>{final} times more likely</strong> than a vaccinated person to end up in ICU.
-
-
-        
+		Thus, an unvaccinated person is {likelyVac}/{likelyUnVac} =
+		<strong>{final} times more likely</strong> than a vaccinated person to die.
 	</Card>
 </main>
 
@@ -157,7 +170,7 @@
 		padding: 0;
 	}
 	form {
-		width: 70%;
+		width: 90%;
 		margin: 0 auto;
 		text-align: right;
 		/* border: 1px solid green; */
